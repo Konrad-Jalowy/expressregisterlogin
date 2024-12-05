78513e3920efeea3bbd52d6f5e6e4e44025be759
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-05 13:51:57 +0100

    another validator

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ app.use(express.json());
 app.get("/", UserController.main);
 app.get('/users', UserController.main);
 
-app.post('/users', UserController.registerValidator);
+app.post("/users", UserController.registerValidator, (req, res, next) => {
+    const errors = validationResult(req)
+  if (errors.isEmpty()) {
+    
+    return next();
+  }
+ return res.status(422).json({errors: errors.array()})
+
+});
 app.post('/users', UserController.register);
 
 app.post("/users/login", UserController.loginValidator, (req, res, next) => {
--- userController.js
+++ userController.js
@@ -40,7 +40,20 @@ exports.loginValidator =  [
     body('password', 'The minimum password length is longer').isLength({min: 3}),
     body('login', 'The minimum login length is 6 characters').isLength({min: 6}),
   ];
-  
+
+exports.registerValidator = [
+    body('login', 'Login cannot be empty').not().isEmpty(),
+    body('password', 'Password cannot be empty').not().isEmpty(),
+    body('password', 'The minimum password length is longer').isLength({min: 3}),
+    body('login', 'The minimum login length is 6 characters').isLength({min: 6}),
+    body('firstName', 'FirstName cannot be empty').not().isEmpty(),
+    body('lastName', 'LastName cannot be empty').not().isEmpty(),
+    body('firstName', 'FirstName must be string').isString(),
+    body('lastName', 'LastName must be string').isString(),
+    body('firstName', 'FirstName must be 6 chars or longer').isLength({min: 6}),
+    body('lastName', 'LastName must be 6 chars or longer').isLength({min: 6}),
+  ];
+
 exports.login = catchAsync(async (req, res, next) => {
   
     let user = await User.findOne({login: req.body.login});
@@ -54,40 +67,40 @@ exports.login = catchAsync(async (req, res, next) => {
         res.send('Not Allowed')
       }
   });
-  exports.registerValidator = (req, res, next) => {
+//   exports.registerValidatorOld = (req, res, next) => {
 
-    const errorList = [];
+//     const errorList = [];
 
-    if(req.body.login === undefined){
-        errorList.push({"login": "You must send login!"});
-    };
-    if(req.body.login === ""){
-        errorList.push({"login": "Login cannot be empty!"});
-    };
-    if(req.body.password === undefined){
-        errorList.push({"password": "You must send password"});
-    };
-    if(req.body.password === ""){
-        errorList.push({"password": "Password cannot be empty!"});
-    };
-    if(req.body.firstName === undefined){
-        errorList.push({"firstName": "You must send firstName"});
-    }
-    if(req.body.firstName === ""){
-        errorList.push({"firstName": "firstName cannot be empty!"});
-    }
-    if(req.body.lastName === undefined){
-        errorList.push({"lastName": "You must send lastName"});
-    }
-    if(req.body.lastName === ""){
-        errorList.push({"lastName": "lastName cannot be empty!"});
-    }
+//     if(req.body.login === undefined){
+//         errorList.push({"login": "You must send login!"});
+//     };
+//     if(req.body.login === ""){
+//         errorList.push({"login": "Login cannot be empty!"});
+//     };
+//     if(req.body.password === undefined){
+//         errorList.push({"password": "You must send password"});
+//     };
+//     if(req.body.password === ""){
+//         errorList.push({"password": "Password cannot be empty!"});
+//     };
+//     if(req.body.firstName === undefined){
+//         errorList.push({"firstName": "You must send firstName"});
+//     }
+//     if(req.body.firstName === ""){
+//         errorList.push({"firstName": "firstName cannot be empty!"});
+//     }
+//     if(req.body.lastName === undefined){
+//         errorList.push({"lastName": "You must send lastName"});
+//     }
+//     if(req.body.lastName === ""){
+//         errorList.push({"lastName": "lastName cannot be empty!"});
+//     }
    
-    if(errorList.length > 0){
-        return res.status(400).json({"Error": errorList});
-    }
-    return next();
-};
+//     if(errorList.length > 0){
+//         return res.status(400).json({"Error": errorList});
+//     }
+//     return next();
+// };
 exports.register = catchAsync(async (req, res, next) => {
     let exists = await User.findOne({login: req.body.login});
     if(exists !== null){
